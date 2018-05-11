744f9d07b4d6dde0e538893b1b81c1dee127ffb6
uniquexiaobai <unique_xiaobai@qq.com>
2018-05-11 12:16:58 +0800

    feature: add phone_book

--- js/pages/discovery/phone_book.js
+++ js/pages/discovery/phone_book.js
@@ -1,5 +1,20 @@
 import React, {Component} from 'react';
-import {View, Text} from 'react-native';
+
+import {Linking, Alert, View} from 'react-native';
+import {List} from 'antd-mobile';
+const {Item} = List;
+
+const persons = [
+    {name: '治安办公室', number: '82426114'},
+    {name: '门诊一部', number: '82426121'},
+    {name: '门诊二部', number: '64884870'},
+    {name: '心理咨询预约', number: '62843779'},
+    {name: '校园卡自助服务', number: '62841188'},
+    {name: '成绩与学籍管理', number: '82426824'},
+    {name: '行政办公', number: '82426830'},
+    {name: '本科生就业', number: '82426857'},
+    {name: '网站维护及技术支持', number: '82426946'},
+];
 
 class PhoneBook extends Component {
     static navigationOptions = ({navigation}) => ({
@@ -7,14 +22,30 @@ class PhoneBook extends Component {
     });
 
     render() {
-        const {navigation} = this.props;
-
         return (
-            <View>
-                <Text>hello {navigation.state.routeName}</Text>
+            <View style={{marginTop: 10}}>
+                <List>
+                    {
+                        persons.map(person => (
+                            <Item onClick={() => this.showDialModal(person.number)} key={person.number}>{person.name}</Item>
+                        ))
+                    }
+                </List>
             </View>
         );
     }
+
+    showDialModal = (number) => {
+        Alert.alert(
+            '',
+            `确定要拨打 ${number}`,
+            [
+              {text: '取消', style: 'cancel'},
+              {text: '确定', onPress: () => Linking.openURL(`tel:${number}`)},
+            ],
+            { cancelable: false }
+          )
+    };
 }
 
 export default PhoneBook;
