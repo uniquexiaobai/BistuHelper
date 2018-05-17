383ed0859902af99121cc22130ad3467bb21c9b9
uniquexiaobai <unique_xiaobai@qq.com>
2018-05-17 23:18:53 +0800

    add bus data

---
+++ js/constants/bus.js
@@ -0,0 +1,106 @@
+export default [
+    {
+        busName: "东线班车",
+        busType: "通勤班车",
+        departureTime: "06:50:00",
+        returnTime: "17:10:00",
+        busLine: [{station: '金台路校区', arrivalTime: '06:50'}, {station: '北二环钟楼北桥', arrivalTime: '07:15'}, {station: '德外关厢', arrivalTime: '07:20'}, {station: '小营校区', arrivalTime: '07:40'}, {station:'小营校区', arrivalTime: '17:10'}, {station:'德外关厢', arrivalTime: '17:25'}, {station: '北二环钟楼北桥', arrivalTime: '17:35'}, {station: '金台路校区', arrivalTime: '19:00'}],
+        busIntro: "东线班车"
+    },
+    {
+        busName: "西线班车",
+        busType: "通勤班车",
+        departureTime: "06:55:00",
+        returnTime: "17:10:00",
+        busLine: [{station: '公主坟', arrivalTime: '06:55'}, {station:'当代商城', arrivalTime: '07:05'}, {station: '蓝旗营清华西南门', arrivalTime: '07:15'}, {station: '小营校区', arrivalTime: '07:45'}, {station: '小营校区', arrivalTime: '17:10'}, {station:'蓝旗营清华西南门', arrivalTime: '17:35'}, {station: '当代商城', arrivalTime: '17:50'}, {station:'公主坟', arrivalTime: '18:20'}],
+        busIntro: "西线班车"
+    },
+    {
+        busName: "望京-小营",
+        busType: "通勤班车",
+        departureTime: "07:05:00",
+        returnTime: "17:10:00",
+        busLine: [{station: '北四环联大', arrivalTime: '07:05'}, {station:'望兴园', arrivalTime: '07:15'}, {station: '望京花园', arrivalTime: '07:20'}, {station: '小营校区', arrivalTime: '07:50'}, {station: '小营校区', arrivalTime: '17:10'}, {station: '望京花园', arrivalTime: '17:30'}, {station: '望兴园', arrivalTime: '17:40'}, {station: '北四环联大', arrivalTime: '17:55'}],
+        busIntro: "望京-小营"
+    },
+    {
+        busName: "回龙观-育新-小营",
+        busType: "通勤班车",
+        departureTime: "07:00:00",
+        returnTime: "17:10:00",
+        busLine: [{station: '流星花园', arrivalTime: '07:00'}, {station: '北店嘉园', arrivalTime: '07:03'}, {station: '矩阵小区', arrivalTime: '07:06'}, {station: '龙跃苑', arrivalTime: '07:10'}, {station: '育新小区', arrivalTime: '07:26'}, {station: '小营校区', arrivalTime: '07:36'}, {station: '小营校区', arrivalTime: '17:10'}, {station: '育新小区', arrivalTime: '17:18'}, {station: '龙跃苑', arrivalTime: '17:35'}, {station: '矩阵小区', arrivalTime: '17:40'}, {station: '北店嘉园', arrivalTime: '17:45'}, {station: '流星花园', arrivalTime: '17:48'}],
+        busIntro: "回龙观-育新-小营"
+    },
+    {
+        busName: "健翔桥-小营",
+        busType: "通勤班车",
+        departureTime: "15:20:00",
+        returnTime: "15:35:00",
+        busLine: [{station: '健翔桥校区', arrivalTime: '15:20'}, {station: '小营校区', arrivalTime: '15:35'}, {station: '小营校区', arrivalTime: '15:35'}, {station :'健翔桥校区', arrivalTime: '16:05'}],
+        busIntro: "健翔桥-小营"
+    },
+    {
+        busName: "小营-健翔桥",
+        busType: "通勤班车",
+        departureTime: "06:30:00",
+        returnTime: "07:15:00",
+        busLine: [{station: '小营校区', arrivalTime: '6:30'}, {station: '健翔桥校区', arrivalTime: '6:55'}, {station: '健翔桥校区', arrivalTime: '7:15'}, {station:'小营校区', arrivalTime: '7:50'}],
+        busIntro: "小营-健翔桥"
+    },
+    {
+        busName: "回龙观-育新-健翔桥",
+        busType: "通勤班车",
+        departureTime: "07:05:00",
+        returnTime: "17:10:00",
+        busLine: [{station: '育新小区', arrivalTime: '07:05'}, {station: '健翔桥校区', arrivalTime: '07:42'}, {station: '健翔桥校区', arrivalTime: '17:10'}, {station:'育新小区', arrivalTime: '17:32'}, {station: '龙跃苑', arrivalTime: '17:50'}],
+        busIntro: "回龙观-育新-健翔桥"
+    },
+    {
+        busName: "望京-健翔桥",
+        busType: "通勤班车",
+        departureTime: "07:15:00",
+        returnTime: "17:10:00",
+        busLine: [{station: '望兴园', arrivalTime: '07:15'}, {station: '望京花园', arrivalTime: '07:17'}, {station: '健翔桥校区', arrivalTime: '07:45'}, {station: '健翔桥校区', arrivalTime: '17:10'}, {station: '望京花园', arrivalTime: '17:45'}, {station: '望兴园', arrivalTime: '17:47'}],
+        busIntro: "望京-健翔桥"
+    },
+    {
+        busName: "小营-清河校区南门-健翔桥",
+        busType: "教学班车",
+        departureTime: "09:50:00",
+        returnTime: "",
+        busLine: [{station: '小营校区', arrivalTime: '09:50'}, {station: '清河小区南门', arrivalTime: '09:51'}, {station: '健翔桥校区', arrivalTime: '10:35'}],
+        busIntro: "小营-清河小区南门-健翔桥"
+    },
+    {
+        busName: "小营-清河校区南门-健翔桥",
+        busType: "教学班车",
+        departureTime: "15:20:00",
+        returnTime: "",
+        busLine: [{station: '小营校区', arrivalTime: '15:20'}, {station: '清河校区南门', arrivalTime: '15:21'}, {station: '健翔桥校区', arrivalTime: '15:40'}],
+        busIntro: "小营-清河校区南门-健翔桥"
+    },
+    {
+        busName: "健翔桥-清河校区南门-小营",
+        busType: "教学班车",
+        departureTime: "12:30:00",
+        returnTime: "",
+        busLine: [{station: '健翔桥校区', arrivalTime: '12:30'}, {station: '清河校区南门', arrivalTime: '12:46'}, {station: '小营', arrivalTime: '12:48'}],
+        busIntro: "健翔桥-清河校区南门-小营"
+    },
+    {
+        busName: "小营-清河校区南门-健翔桥",
+        busType: "教学班车",
+        departureTime: "12:30:00",
+        returnTime: "",
+        busLine: [{station:'小营校区', arrivalTime: '12:30'}, {station: '清河校区南门', arrivalTime: '12:33'}, {station: '健翔桥校区', arrivalTime: '12:55'}],
+        busIntro: "小营-清河校区南门-健翔桥"
+    },
+    {
+        busName: "健翔桥-小营",
+        busType: "教学班车",
+        departureTime: "09:50:00",
+        returnTime: "",
+        busLine: [{station: '健翔桥校区', arrivalTime: '09:50'}, {station:'小营校区', arrivalTime: '10:10'}],
+        busIntro: "健翔桥-小营"
+    }
+];
