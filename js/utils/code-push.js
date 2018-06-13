75b0e42de78c99a8db6cbf52abbcf662300f4b71
uniquexiaobai <unique_xiaobai@qq.com>
2018-06-13 20:27:42 +0800

    codePush bug

--- js/utils/code-push.js
+++ js/utils/code-push.js
@@ -3,6 +3,6 @@ import CodePush from 'react-native-code-push';
 export const CodePush__sync = () => {
     CodePush.sync({
         updateDialog: true,
-        installMode: codePush.InstallMode.IMMEDIATE
+        installMode: CodePush.InstallMode.IMMEDIATE
     });
 };
